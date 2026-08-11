1ef079ad21da55bbff696788e41277d81b30ee6d
wang <2010546694@qq.com>
2026-08-11 13:24:35 +0800

    test: count UTF-8 bytes in ephemeral limits

--- tests/test_phase11d_pilot_v3_c08.py
+++ tests/test_phase11d_pilot_v3_c08.py
@@ -8,6 +8,10 @@ class Phase11DPilotV3C08Tests(unittest.TestCase):
         with self.assertRaisesRegex(gate_b.GateBExecutorError, "pilot_plan_redaction_failure"):
             gate_b._hash_ephemeral_text("pilot_plan", "abcde", maximum_bytes=4)
 
+    def test_ephemeral_size_limit_counts_utf8_bytes(self) -> None:
+        with self.assertRaisesRegex(gate_b.GateBExecutorError, "pilot_plan_redaction_failure"):
+            gate_b._hash_ephemeral_text("pilot_plan", "\u6d4b\u8bd5", maximum_bytes=5)
+
 
 if __name__ == "__main__":
     unittest.main()
